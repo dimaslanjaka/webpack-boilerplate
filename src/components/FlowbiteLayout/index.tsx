d8c66db2796ee9b861c7f716791a3e9dfcd6eb93
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-03 15:20:38 +0700

    add toast context

--- src/components/FlowbiteLayout/index.tsx
+++ src/components/FlowbiteLayout/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import { Fancybox } from '@fancyapps/ui';
+import { Toast } from 'flowbite-react';
+import React, { useState } from 'react';
 import { BiBuoy } from 'react-icons/bi';
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
+import {
+  HiArrowSmRight,
+  HiChartPie,
+  HiFire,
+  HiInbox,
+  HiShoppingBag,
+  HiTable,
+  HiUser,
+  HiViewBoards
+} from 'react-icons/hi';
 import { Outlet } from 'react-router-dom';
 import FlowbiteContext from './context/FlowbitContext';
 import { SidebarProvider } from './context/SidebarContext';
 import Header from './header';
 import Sidebar from './sidebar';
-import { Fancybox } from '@fancyapps/ui';
 
-function FlowbiteLayout(props?: { children: JSX.Element | React.ReactNode }) {
+export interface FlowbiteToastContext {
+  setShowToast: (show: boolean) => void;
+  setToastInfo: (info: { title: string; description: string }) => void;
+}
+
+export function FlowbiteLayout(props?: { children: JSX.Element | React.ReactNode }) {
+  const [showToast, setShowToast] = useState(false);
+  const [toastInfo, setToastInfo] = useState({ title: 'toast title', description: 'toast description' });
+
   React.useEffect(() => {
     const wrapper = document.getElementById('router-wrapper');
     if (wrapper) {
@@ -42,8 +60,22 @@ function FlowbiteLayout(props?: { children: JSX.Element | React.ReactNode }) {
             ) : (
               <div className="flex flex-col md:flex-row dark:bg-gray-900">
                 <div className="order-2 mx-4 mt-4 mb-24 flex-[1_0_16rem]" id="router-wrapper">
-                  <Outlet />
+                  <Outlet context={{ setShowToast, setToastInfo }} />
                 </div>
+                {showToast && (
+                  <Toast className="absolute top-5 end-5 z-50 shadow">
+                    <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-cyan-100 text-cyan-500 dark:bg-cyan-800 dark:text-cyan-200">
+                      <HiFire className="h-5 w-5" />
+                    </div>
+                    <div className="ml-3 text-sm font-normal">
+                      <span className="mb-1 text-sm font-semibold text-gray-900 dark:text-white">
+                        {toastInfo.title}
+                      </span>
+                      <div className="mb-2 text-sm font-normal">{toastInfo.description}</div>
+                    </div>
+                    <Toast.Toggle onDismiss={() => setShowToast(false)} />
+                  </Toast>
+                )}
                 <div className="order-1">
                   <ActualSidebar />
                 </div>
@@ -76,10 +108,10 @@ function ActualSidebar(): JSX.Element {
           <Sidebar.Item href="#" icon={HiShoppingBag}>
             Products
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+          <Sidebar.Item href="/login" icon={HiArrowSmRight}>
             Sign In
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiTable}>
+          <Sidebar.Item href="/login" icon={HiTable}>
             Sign Up
           </Sidebar.Item>
         </Sidebar.ItemGroup>
@@ -100,7 +132,6 @@ function ActualSidebar(): JSX.Element {
 }
 
 export default FlowbiteLayout;
-export { FlowbiteLayout as Component };
 
 export function FlowbiteLayoutWithoutSidebar() {
   return (
