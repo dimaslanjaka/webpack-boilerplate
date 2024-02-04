3825b1079127d4bc448695a61a0a12a2142739a7
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-05 06:28:35 +0700

    update spring utility

--- src/utils/springUtils.ts
+++ src/utils/springUtils.ts
@@ -8,9 +8,25 @@ export interface AjaxResponse {
 
 export default class springUtils {
   private static origin: URL;
+  /**
+   * get url origin of backend
+   * @returns URL instance without pathname
+   */
   public static getOrigin() {
     if (typeof this.origin === 'undefined') this.origin = new URL(env.BASE_API);
+    this.origin.pathname = '/';
     return this.origin;
+  }
+  /**
+   * get url of backend
+   * @param pathname pathname of backend
+   * @returns URL instance
+   */
+  public static getURL(pathname?: string) {
+    this.getOrigin();
+    const url = this.origin;
+    if (typeof pathname == 'string') url.pathname = pathname;
+    return url;
   }
   public static checkLogin(redirect = true) {
     const url = this.getOrigin();
